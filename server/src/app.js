3e2be4b3b33bf617ac4820cd208df82fdcd29654
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-02-12 14:11:56 +0530

    Implemented errorHandler middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 // Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome To API!" });
 });
+
+// Error handler
+const errorHandler = require("./middleware/errorHandler");
+app.use(errorHandler);
 
 module.exports = app;
